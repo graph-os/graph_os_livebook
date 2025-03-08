75127968d834791863102d735c16fce08f185bab
Vegard Krogh <vegardkrr@gmail.com>
2025-03-08 21:32:44 +0100

    Initial commit of graph_os_livebook

---
+++ lib/graph_os_livebook.ex
@@ -0,0 +1,18 @@
+defmodule GraphOsLivebook do
+  @moduledoc """
+  Documentation for `GraphOsLivebook`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GraphOsLivebook.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/graph_os_livebook_test.exs
@@ -0,0 +1,8 @@
+defmodule GraphOsLivebookTest do
+  use ExUnit.Case
+  doctest GraphOsLivebook
+
+  test "greets the world" do
+    assert GraphOsLivebook.hello() == :world
+  end
+end
